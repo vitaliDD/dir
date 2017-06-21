31ebe858b3571ef0d31ec876eeebd830fa6c299b
Doshchechko <Doshchechko@A418b-1.bms.by>
2017-06-21 14:53:36 +0300

    well done

--- dz13/src/dz13/Dz13.java
+++ dz13/src/dz13/Dz13.java
@@ -1,18 +1,23 @@
-
-
 package dz13;
 
-/**
- *
- * @author ya
- */
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+
 public class Dz13 {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static final String FILENAME = "test.json";
+
     public static void main(String[] args) {
-       
+        ObjectMapper mapper = new ObjectMapper();
+        Root root = null;
+        try {
+            root = mapper.readValue(new File(FILENAME), Root.class);
+
+        } catch (Exception ex) {
+            System.out.println("oshibka " + ex.toString());
+        }
+        System.out.println(root.toString());
+
     }
 
 }
---
+++ dz13/src/dz13/People.java
@@ -0,0 +1,63 @@
+package dz13;
+
+//import com.google.gson.annotations.SerializedName;
+
+
+
+public class People {
+    
+private int age;
+private int id;
+private boolean IsDegree;
+private String name;
+private String surname;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isIsDegree() {
+        return IsDegree;
+    }
+
+    public void setIsDegree(boolean IsDegree) {
+        this.IsDegree = IsDegree;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" + "age=" + age + ", id=" + id + ", IsDegree=" + IsDegree + ", name=" + name + ", surname=" + surname + '}';
+    }
+
+
+    
+    
+}
