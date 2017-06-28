d507022dd908a62240f403df0840fafe41437e9e
vitaliDD <vitali.doshch@gmail.com>
2017-06-28 17:01:54 +0300

    done

--- dz16/src/dz16/MyThread.java
+++ dz16/src/dz16/MyThread.java
@@ -4,12 +4,12 @@ public class MyThread extends Thread {
 
     public void run() {
 
-        print10();
+        print10(this);
     }
 
-    private  synchronized void print10() {
+    private static synchronized void print10(Thread th) {
         for (int i = 0; i < 10; i++) {
-            System.out.print(this.getName()+" ");
+            System.out.print(th.getName()+" ");
 
             for (int j = 1; j < 10; j++) {
 
