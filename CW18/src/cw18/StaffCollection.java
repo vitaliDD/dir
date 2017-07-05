f2c350c19c765d1aff4dec0ff5cb7ed446404d52
vitaliDD <vitali.doshch@gmail.com>
2017-07-05 18:56:14 +0300

    aa

--- CW18/src/cw18/StaffCollection.java
+++ CW18/src/cw18/StaffCollection.java
@@ -1,18 +1,46 @@
 package cw18;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class StaffCollection<T extends People>{
     
-    
-    private ArrayList<T> list=new ArrayList<>();
+    private HashMap<String, List <T>> data = new HashMap<>();
+    private T director;
+    //private ArrayList<T> list=new ArrayList<>();
     
     
     public void add(T t){
     
     list.add(t);
     }
+    public void add(String department, T value){
+        if(data.containsKey(department)){
+        
+            data.get(department).add(value);
+        
+        }
+        else{
+        
+            List<T> list =data.get(department);
+            if(list==null){
+                list=new ArrayList<>();
+                
+            }
+            list.add(value);
+            data.put(department, list);
+        }
+
+    }
     
+    public void addDirector(T value){
+        director=value;
+    }
+    
+    public T getDirector(){
+        return director;
+    }
     
     public void remove(T t){
     
