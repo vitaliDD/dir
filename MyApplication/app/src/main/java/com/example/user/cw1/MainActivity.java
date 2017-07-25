3afa4030e8baf1f9ecee0c3b7532d9ae626e75e7
vitaliDD <vitali.doshch@gmail.com>
2017-07-26 00:59:03 +0300

    hw1

--- MyApplication/app/src/main/java/com/example/user/cw1/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/user/cw1/MainActivity.java
@@ -7,30 +7,41 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 /**
  * Created by user on 24.07.2017.
  */
-public class MainActivity extends Activity  {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 
     private Button helloButton;
-    private Button byeButton;
-    private int i;
+    private TextView text1;
+    private TextView text2;
+    String strChange;
+    View.OnClickListener onClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            changeText();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("newActivity","onCreate");
         helloButton=(Button)findViewById(R.id.helloButton);
-        //helloButton.setText("fdf");
-        byeButton=(Button)findViewById(R.id.byeButton);
+        text1=(TextView) findViewById(R.id.text1);
+        text2=(TextView) findViewById(R.id.text2);
 
+        text1.setOnClickListener(onClickListener);
+        text2.setOnClickListener(this);
         helloButton.setOnClickListener(new View.OnClickListener()  {
             @Override
             public void onClick(View v) {
-                i++;
-                helloButton.setText("Button "+i);
+
+                changeText();
             }
         });
     }
@@ -66,4 +77,16 @@ public class MainActivity extends Activity  {
     }
 
 
+    @Override
+    public void onClick(View v) {
+       changeText();
+    }
+
+    private void changeText(){
+
+        strChange=(String)text1.getText();
+        text1.setText(text2.getText());
+        text2.setText(strChange);
+
+    }
 }
